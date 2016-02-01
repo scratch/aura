aeec91062233a5f4b3af66e488d6af41a81da571
Krishnan.N <scratch@srijan.in>
2016-02-01 15:39:20 +0530

    Check if repeater JS init has completed and then look for the different ID's. Works for Init, need to add code for repeated forms.

--- wp-content/themes/aura/js/aura_descr.js
+++ wp-content/themes/aura/js/aura_descr.js
@@ -1,5 +1,17 @@
-jQuery(document).ready(function($)  {
-      $('#input_3_7-1-1').on('change', function() {
-      alert("pos color");
-  });
+var au_id_prefix = "input_";
+var au_bod_col = "1_7";
+var au_pos_col = "1_7";
+var au_neg_col = "1_9";
+var au_repeater_idx = 1;
+
+jQuery(document).on('gform_repeater_init_done', function() {
+	jQuery('#' + au_id_prefix + au_pos_col + '-1-' + au_repeater_idx ).on('change', function() {
+		alert("pos color");
+	});
 });
+
+// While each new form is repeated, get the values for the newly created # ID's.
+// gform_repeater_after_repeat
+//	  ++au_repeater_idx;
+
+jQuery(document).on('gform_repeater_init_done', function() {
