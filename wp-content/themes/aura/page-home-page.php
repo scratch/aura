84d4dd174731397f822162220204475f32ac1949
Krishnan.N <scratch@srijan.in>
2016-03-19 15:08:33 +0530

    Use wp_loginout() rather than older code to both provide login/logout link, as well as redirect user to home page upon login.

--- wp-content/themes/aura/page-home-page.php
+++ wp-content/themes/aura/page-home-page.php
@@ -11,8 +11,13 @@
 get_header();
 
 if (!is_user_logged_in()) {
+  $aura_home=get_option('siteurl');
 ?>
+<p> Please <?php wp_loginout($aura_home); ?>  </p>
+<!--
 <p style="text-align: left;"><a style="text-align: center;" href="http://aura.gailabs.com/wp-login.php" target="_blank">Login </a>here</p>
+-->
+
 <?php
 }
 get_sidebar();
@@ -20,5 +25,5 @@ get_footer();
 ?>
 
 <ul class="headerlinks">
-	<li><?php wp_loginout(); ?></li>
+  <li><?php $aura_home = get_option('siteurl'); wp_loginout($aura_home); ?></li>
 </ul>
